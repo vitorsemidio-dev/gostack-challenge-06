ec0584382d24442dd4cc31dcc8b995ff0e5a155c
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-07-19 14:37:12 -0300

    Persistência no banco

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,50 @@
 // import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
 
 import Transaction from '../models/Transaction';
 
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CategoriesRepository from '../repositories/CategoriesRepository';
+
+interface IRequest {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: IRequest): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoriesRepository = getCustomRepository(CategoriesRepository);
+
+    let categoryAlreadyExists = await categoriesRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!categoryAlreadyExists) {
+      categoryAlreadyExists = categoriesRepository.create({
+        title: category,
+      });
+    }
+
+    await categoriesRepository.save(categoryAlreadyExists);
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id: categoryAlreadyExists.id,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
