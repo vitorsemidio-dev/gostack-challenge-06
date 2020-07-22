e73ddbc81546ab966196091f785461adc87d7672
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-07-21 21:16:03 -0300

    upload arquivo csv e mapeamento de dados

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
 import { getCustomRepository } from 'typeorm';
+
+import uploadConfig from '../config/upload';
 
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
 import DeleteTransactionService from '../services/DeleteTransactionService';
-// import ImportTransactionsService from '../services/ImportTransactionsService';
+import ImportTransactionsService from '../services/ImportTransactionsService';
 
+const upload = multer(uploadConfig);
 const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
@@ -51,8 +55,15 @@ transactionsRouter.delete('/:id', async (request, response) => {
   }
 });
 
-transactionsRouter.post('/import', async (request, response) => {
-  return response.json();
-});
+transactionsRouter.post(
+  '/import',
+  upload.single('file'),
+  async (request, response) => {
+    const importTransactionsService = new ImportTransactionsService();
+
+    const transactions = importTransactionsService.execute(request.file.path);
+    return response.json(transactions);
+  },
+);
 
 export default transactionsRouter;
--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,38 @@
+import csvParse from 'csv-parse';
+import fs from 'fs';
+
 import Transaction from '../models/Transaction';
 
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
+  async execute(filePath: string): Promise<Transaction[]> {
     // TODO
+    const contactsReadStream = fs.createReadStream(filePath);
+
+    const parses = csvParse({
+      from_line: 2,
+    });
+
+    const transactions = [];
+    const categories = [];
+
+    const parseCSV = contactsReadStream.pipe(parses);
+
+    parseCSV.on('data', async line => {
+      const [title, type, value, category] = line.map((cell: string) =>
+        cell.trim(),
+      );
+
+      if (!title || !type || !value) {
+        return;
+      }
+
+      categories.push(category);
+      transactions.push({ title, type, value, category });
+    });
+
+    await new Promise(resolve => parseCSV.on('end', resolve));
+    console.log(categories);
+    console.log(transactions);
   }
 }
 
